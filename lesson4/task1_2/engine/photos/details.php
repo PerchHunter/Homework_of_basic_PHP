d534423847ed12c00a184475f43d121539379422
PerchHunter <bbldyk@mail.ru>
2022-04-05 18:22:44 +0500

    Revert "Домашнее задание к лекции 4 (без дополнительного от преподавателя)"
    This reverts commit bd500017d35f547a5caeabbe4d71626bc7a6d4fe.

--- lesson4/task1_2/engine/photos/details.php
+++
@@ -1,8 +0,0 @@
-<?php
-if($_GET['name']):?>
-       <img  style="width: 500px" src="../../public/images/<?=$_GET["name"]?>" alt="photo"/>
-    <?php
-    endif;
-    ?>
-
-    <a style="text-decoration: none; color: black" href="<?= $_SERVER['HTTP_REFERER']?>"><p>Вернуться назад</p></a>
--- lesson4/task3/engine/photos/details.php
+++
@@ -1,8 +0,0 @@
-<?php
-if($_GET['name']):?>
-       <img  style="width: 500px" src="../../public/images/<?=$_GET["name"]?>" alt="photo"/>
-    <?php
-    endif;
-    ?>
-
-    <a href="<?= $_SERVER['HTTP_REFERER']?>">Вернуться назад</a>
